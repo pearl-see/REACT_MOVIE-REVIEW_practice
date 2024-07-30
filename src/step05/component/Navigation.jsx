47d933ba419f613f173d29a1955a428c4ac2611e
pearl <kjj0837@naver.com>
2024-07-30 15:27:29 +0900

    04.Home,About,Detail 완

---
+++ src/step05/component/Navigation.jsx
@@ -0,0 +1,9 @@
+import './Navigation.css';
+export function Navigation(){
+  return(
+    <div className='nav'>
+      <a href="/">Home</a>
+      <a href="/About">About</a>
+    </div>
+  );
+}
---
+++ src/step05/pages/Detail.jsx
@@ -0,0 +1,10 @@
+
+
+import './Detail.css';
+export function Detail(){
+  return(
+    <div className='detail'>
+      <span>상세페이지</span>
+    </div>
+  );
+}
